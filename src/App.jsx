6d267b7202910be781e5b20bf885e3fa737ec981
abhishekjaiswal07890 <abhishekjaiswal07890@gmail.com>
2024-11-04 12:52:29 +0000

    done with seo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import Helmet from 'react-helmet'
 
 import './App.css'
 import Hunters from "./Hunters.jsx"
@@ -14,6 +15,27 @@ function App() {
 return (
   <div >
     <Hunters />
+    <Helmet>
+      <title>HackHive</title>
+      <meta name="description" content="Join us for the Indore Hackathon 2024! Collaborate with talented developers, designers, and entrepreneurs to create innovative solutions."/>
+        <meta name="keywords" content="Indore Hackathon, hackathon 2024, coding competition, tech event, innovate, collaborate, software development, programming, Indore events"/>
+
+      
+        <meta property="og:title" content="Indore Hackathon 2024 | Innovate, Collaborate, Win!" />
+        <meta property="og:description" content="Join us for the Indore Hackathon 2024! Collaborate with talented developers, designers, and entrepreneurs to create innovative solutions." />
+        <meta property="og:image" content="URL_to_image.jpg" /> 
+        <meta property="og:url" content="https://www.yourhackathonwebsite.com" /> 
+        <meta property="og:type" content="website" />
+
+        
+        <meta name="twitter:title" content="Indore Hackathon 2024 | Innovate, Collaborate, Win!"/>
+        <meta name="twitter:description" content="Join us for the Indore Hackathon 2024! Collaborate with talented developers, designers, and entrepreneurs to create innovative solutions."/>
+    
+
+        
+        <meta name="author" content="Indore Hackathon Team"/>
+        <meta name="robots" content="index, follow"/>
+    </Helmet>
   </div>
 );
 
