7dea2c2debf1b2ecb50537b4efd7d2ace78c19f0
abhishekjaiswal07890 <abhishekjaiswal07890@gmail.com>
2024-11-04 11:58:10 +0000

    ga init

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import { useState } from 'react'
 
 import './App.css'
 import Hunters from "./Hunters.jsx"
+import ReactGA from 'react-ga'
+
 
 
 function App() {
+  useEffect(() => {
+        ReactGA.initialize('G-5KSKGREJXQ');
+        ReactGA.pageview(window.location.pathname + window.location.search);
 
-
+          }, []);
+  
   return (
     <div >
        <Hunters />
