805d4b0ba1bb0182d01aabe1673ae4ba330a49f7
abhishekjaiswal07890 <abhishekjaiswal07890@gmail.com>
2024-11-04 12:25:09 +0000

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,33 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import './App.css'
 import Hunters from "./Hunters.jsx"
-import ReactGA from 'react-ga'
+import ReactGA from 'react-ga4'
+const gaId = 'G-DLND0RK08H'
 
-
+ReactGA.initialize(gaId);
 
 function App() {
-  useEffect(() => {
-        ReactGA.initialize('G-5KSKGREJXQ');
-        ReactGA.pageview(window.location.pathname + window.location.search);
 
-          }, []);
-  
-  return (
-    <div >
-       <Hunters />
-    </div>
-  );
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname);
+  }, []);
+
+  const handleButtonClick = () => {
+    ReactGA.event({
+      category: 'User',
+      action: 'Clicked Button',
+      label: 'Home Page Button',
+    });
+    alert('Button clicked!'); // For testing
+  };
+
+
+return (
+  <div >
+    <Hunters />
+  </div>
+);
 
 }
 
